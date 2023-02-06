7dbe6c7f695aa002956767ae6dc7637bb22f4473
LaurynasMaz <laurynasmazeika3@gmail.com>
2023-02-06 13:18:23 +0200

    sukuriami authentication failiai data ir context

---
+++ src/contexts/UserContext.jsx
@@ -0,0 +1,20 @@
+import { createContext } from "react";
+
+const UserContext = createContext ();
+
+const UserProvider = ({ children }) =>{
+
+    return(
+       <UserContext.Provider
+         value={{
+
+         }}
+
+       >
+        {children}
+       </UserContext.Provider> 
+    );
+};
+
+export { UserProvider };
+export default UserContext;
