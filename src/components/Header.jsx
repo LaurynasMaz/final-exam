da7e07d912e468be0e23b61d318e0d53bd0ad0be
LaurynasMaz <laurynasmazeika3@gmail.com>
2023-02-06 15:36:07 +0200

    Padaryta kad butu matomas prisijunges zmogus

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,31 @@
 import { Link, Outlet } from 'react-router-dom';
+import UserInfo from '../User/UserInfo';
+import UserContext from "../contexts/UserContext";
+import { useContext } from "react";
+
+
 const Header = () => {
-    return ( 
-         <>
-          <div className="loginRegister">
-             <Link to='/login'>Login</Link>
-             <Link to='/register'>Register</Link>
-             <Link to='/'>Home</Link>
-          </div>
-          <Outlet />
-         </>
-      );
+
+   const { loggedInUser } = useContext(UserContext);
+
+   return (
+      <>
+         {
+            loggedInUser 
+            ? 
+               <>
+               <UserInfo /> 
+               <Link to='/'>Home</Link>
+               </>
+            :
+               <div className="loginRegister">
+                  <Link to='/login'>Login</Link>
+                  <Link to='/register'>Register</Link>
+               </div>
+         }
+         <Outlet />
+      </>
+   );
 }
  
 export default Header;
