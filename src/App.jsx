8f0873a15a623da79898822fafffe71dfaec1b4e
LaurynasMaz <laurynasmazeika3@gmail.com>
2023-02-06 13:51:12 +0200

    uzdedami rautes

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,21 @@ import { Routes, Route } from 'react-router-dom';
 import Login from './authentication/Login';
 import Register from './authentication/Register'
 import Header from './components/Header';
+import Home from './components/Home';
 
 const App = () => {
    return (
-      <>
+   <>
       <div>
          <Routes>
             <Route element={<Header />}>
                <Route path="/register" element={<Register />}/>
                <Route path="/login" element={<Login />}/>
+               <Route path='/' element={<Home />}/>
             </Route>
          </Routes>
-         </div>
-      </>
+      </div>
+   </>
    );
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,15 @@
 import { Link, Outlet } from 'react-router-dom';
 const Header = () => {
-   return ( 
-      <>
-       <div className="loginRegister">
-         <Link to='/login'>Login</Link>
-         <Link to='/register'>Register</Link>
-       </div>
-       <Outlet />
-      </>
-    );
+    return ( 
+         <>
+          <div className="loginRegister">
+             <Link to='/login'>Login</Link>
+             <Link to='/register'>Register</Link>
+             <Link to='/'>Home</Link>
+          </div>
+          <Outlet />
+         </>
+      );
 }
  
 export default Header;
