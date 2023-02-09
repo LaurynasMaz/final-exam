ce28de825b7b8ae6fa310f3349091fab1d92ae45
LaurynasMaz <laurynasmazeika3@gmail.com>
2023-02-09 15:02:52 +0200

    padarytas postu sort by date

--- src/components/posts/Posts.jsx
+++ src/components/posts/Posts.jsx
@@ -1,15 +1,27 @@
 import PostContext from "../../contexts/PostContext";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import Post from "./Post";
 
 const Posts = () => {
-
    const { posts } = useContext(PostContext);
+   const [sortBy, setSortBy] = useState("oldest");
+
+   const sortedPosts = [...posts].sort((a, b) => {
+      if (sortBy === "oldest") {
+         return new Date(a.timeStamp) - new Date(b.timeStamp);
+      } else {
+         return new Date(b.timeStamp) - new Date(a.timeStamp);
+      }
+   });
 
    return (
       <>
+         <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+            <option value="oldest">From Oldest to Newest</option>
+            <option value="newest">From Newest to Oldest</option>
+         </select>
          {
-            posts.map(post => 
+            sortedPosts.map(post => 
                <Post 
                   key={post.id}
                   data={post}
